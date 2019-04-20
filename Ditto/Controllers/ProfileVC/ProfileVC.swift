62dd54aefc6554e640fd16aca1aaa61dd559eb4a
Melanie Cooray <cooraymelanie@gmail.com>
2019-04-19 18:11:44 -0700

    mels

--- Ditto/Controllers/ProfileVC/ProfileVC.swift
+++ Ditto/Controllers/ProfileVC/ProfileVC.swift
@@ -22,10 +22,12 @@ class ProfileViewController: UIViewController {
     var customSC: UISegmentedControl!
     var tableView: UITableView!
     
-    var playlists: [String] = ["vibe station", "spring 2019 jams", "bucket list: songs we must listen to", "econ 100a ~lit~ study group"]
-    var played: [String] = ["17h", "2d", "6h", "8d"]
-    var playlistTitleList : [String] = ["bucket list: songs we must listen to", "vibe station", "spring 2019 jams", "econ100a ~lit~ study group", "triple bffl favorites"]
-    var playlistLastPlayed : [String] = ["last played: 6h", "last played: 17h", "last played: 17h", "last played: 2d", "last played: 8d", "last played: 9d"]
+    //var playlists: [String] = ["vibe station", "spring 2019 jams", "bucket list: songs we must listen to", "econ 100a ~lit~ study group"]
+    //var played: [String] = ["17h", "2d", "6h", "8d"]
+    var playlistTitleList : [String] = []
+    //var playlistTitleList : [String] = ["bucket list: songs we must listen to", "vibe station", "spring 2019 jams", "econ100a ~lit~ study group", "triple bffl favorites"]
+    var playlistLastPlayed : [String] = ["last played: 6h"]
+    //var playlistLastPlayed : [String] = ["last played: 6h", "last played: 17h", "last played: 17h", "last played: 2d", "last played: 8d", "last played: 9d"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,8 +47,10 @@ class ProfileViewController: UIViewController {
                 
                 let value = snapshot.value as? NSDictionary
                 let retrievedName = value?["Name"] as? String
+                self.playlistTitleList = value?["playlists"] as! [String]
                 print("fullnameretrieved")
                 print(retrievedName)
+                self.tableView.reloadData()
                 self.nameLabel.text = retrievedName
             } else {
                 print(currentID)
