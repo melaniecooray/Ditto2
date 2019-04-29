4666d7736bdc075fc49e46a204e18e117f2377ff
Sam Lee <samanthaslee@berkeley.edu>
2019-04-28 23:35:54 -0700

    fixed the profile tables again

--- Ditto/Controllers/ProfileVC/ProfileVC.swift
+++ Ditto/Controllers/ProfileVC/ProfileVC.swift
@@ -68,6 +68,10 @@ class ProfileViewController: UIViewController {
         customSC.selectedSegmentIndex = 0
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        customSC.selectedSegmentIndex = 0
+    }
+    
     func getUserInformation() {
         print(currentID)
         print("just tried to print current id")
